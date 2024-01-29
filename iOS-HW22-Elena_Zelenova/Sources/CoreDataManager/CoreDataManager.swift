ca83b29f396a125c1ac3568974dce538900d614d
elzel <elzel666@gmail.com>
2024-01-29 10:52:23 +0300

    added_CoreDataManager_functions

--- iOS-HW22-Elena_Zelenova/Sources/CoreDataManager/CoreDataManager.swift
+++ iOS-HW22-Elena_Zelenova/Sources/CoreDataManager/CoreDataManager.swift
@@ -1,8 +1,39 @@
-//
-//  CoreDataManager.swift
-//  iOS-HW22-Elena_Zelenova
-//
-//  Created by Helena on 16.01.2024.
-//
-
 import Foundation
+import CoreData
+
+class CoreDataManager {
+    var users: [NSManagedObject] = []
+    
+    private let request: NSFetchRequest<User> = User.fetchRequest()
+    
+    func save(_ withName: String) {
+        let user = User(context: AppDelegate.viewContext)
+        user.name = withName
+    }
+    
+    func fetchAllUsers(_ withName: String) -> [User]? {
+        do {
+            let users = try AppDelegate.viewContext.fetch(request)
+            return users
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+    
+    func fetchUsers(withName: String) -> [User]? {
+        request.predicate = NSPredicate(format: "name == %@", withName)
+        
+        do {
+            let users = try AppDelegate.viewContext.fetch(request)
+            return users
+        } catch {
+            print(error)
+            return nil
+        }
+    }
+    
+    func deleteUser(user: User) {
+        AppDelegate.viewContext.delete(user)
+    }
+}
